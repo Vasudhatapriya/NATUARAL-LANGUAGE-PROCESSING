ac0ae29c19ded5c84802d7e8a1732f340baf7f95
Vasudhatapriya <44346955+Vasudhatapriya@users.noreply.github.com>
2019-12-07 17:36:02 +0530

    Add files via upload
    The first task involves using the anaconda software and do some basic coding in it.

---
+++ TASK1.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+'Shakespare gave the world best of theatrics'
+
+
+# In[2]:
+
+
+'''Shakespare gave the world best of theatrics'''
+
+
+# In[3]:
+
+
+'Shakespare didn't gave the world best of theatrics'
+
+
+# In[4]:
+
+
+'Shakespare didn\'t gave the world best of theatrics'
+
+
+# In[5]:
+
+
+"William Shakespeare (bapt. 26 April 1564 – 23 April 1616)[a] was an English poet, playwright, and actor, widely regarded as the greatest writer in the English language and the world's greatest dramatist.[2][3][4] He is often called England's national poet and the "Bard of Avon" (or simply "the Bard").[5][b] His extant works, including collaborations, consist of some 39 plays,[c] 154 sonnets, two long narrative poems, and a few other verses, some of uncertain authorship. His plays have been translated into every major living language and are performed more often than those of any other playwright.[7]"
+
+
+# In[6]:
+
+
+'''William Shakespeare (bapt. 26 April 1564 – 23 April 1616)[a] was an English poet, playwright, and actor, widely regarded as the greatest writer in the English language and the world's greatest dramatist.[2][3][4] He is often called England's national poet and the "Bard of Avon" (or simply "the Bard").[5][b] His extant works, including collaborations, consist of some 39 plays,[c] 154 sonnets, two long narrative poems, and a few other verses, some of uncertain authorship. His plays have been translated into every major living language and are performed more often than those of any other playwright.[7]'''
+
+
+# In[7]:
+
+
+'विलियम शेक्सपीयर (William Shakespeare ; 23 अप्रैल 1564 (बपतिस्मा हुआ) – 23 अप्रैल 1616 ) अंग्रेजी के कवि, काव्यात्मकता के विद्वान नाटककार तथा अभिनेता थे।[1] उनके नाटकों का लगभग सभी प्रमुख भाषाओं में अनुवाद हुआ है।'
+
+
+# In[8]:
+
+
+"विलियम शेक्सपीयर (William Shakespeare ; 23 अप्रैल 1564 (बपतिस्मा हुआ) – 23 अप्रैल 1616 ) अंग्रेजी के कवि, काव्यात्मकता के विद्वान नाटककार तथा अभिनेता थे।[1] उनके नाटकों का लगभग सभी प्रमुख भाषाओं में अनुवाद हुआ है।"
+
+
+# In[9]:
+
+
+'''विलियम शेक्सपीयर (William Shakespeare ; 23 अप्रैल 1564 (बपतिस्मा हुआ) – 23 अप्रैल 1616 ) अंग्रेजी के कवि, काव्यात्मकता के विद्वान नाटककार तथा अभिनेता थे।[1] उनके नाटकों का लगभग सभी प्रमुख भाषाओं में अनुवाद हुआ है।'''
+
+
+# In[10]:
+
+
+''विलियम शेक्सपीयर (William Shakespeare ; 23 अप्रैल 1564 (बपतिस्मा हुआ) – 23 अप्रैल 1616 ) अंग्रेजी के कवि, काव्यात्मकता के विद्वान नाटककार तथा अभिनेता थे।[1] उनके नाटकों का लगभग सभी प्रमुख भाषाओं में अनुवाद हुआ है।''
+
+
+# In[11]:
+
+
+"Hello,I am Vasudha"
+
+
+# In[ ]:
+
+
+
+
